7c9076d43e3f1fc394425bb76255d971ba02dab3
Utkarsh Priyam <upriyam@cs.cmu.edu>
2024-10-08 15:04:33 -0400

    add mps

--- simple-ML-baseline/train.py
+++ simple-ML-baseline/train.py
@@ -11,7 +11,13 @@ from torch.optim import AdamW
 from tqdm import tqdm
 
 # %%
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    device = 'cuda'
+elif torch.backends.mps.is_available():
+    device = 'mps'
+else:
+    device = 'cpu'
+device = torch.device(device)
 print("Using device:", device)
 
 # %%
